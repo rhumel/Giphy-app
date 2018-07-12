85ae18e8d15a2ebb5ebb1c2125c52924231e85d0
rhumel <rhumel@hotmail.com>
2018-07-12 16:48:22 -0400

    update code to include include ajax call, and image placement

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,59 +2,94 @@ $(document).ready(function () {
 
 // Initial array of Gifs
  var gifArray = ["Dog", "Cat", "Hello", "Happy Birtday"];
-
+ var addedGif;  
  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=HkjK4ZbPCa08m82JFOGCEzAuICSYLnwQ&q="
-  +addedGif + "&limit=10&offset=0&rating=G&lang=en";
+ +addedGif + "&limit=1&offset=0&rating=G&lang=en";
+  
 
+ 
+ 
 
-  var addedGif;  
-  var gifSearch = $("#gif-input").val().trim();
-  console.log(gifSearch);
-  console.log(addedGif);
   
   // handles events where a GIF Search button is clicked
     $("#add-gif").on("click", function(event) {
       event.preventDefault();
+      
       // This line grabs the input from the textbox
       addedGif = $("#gif-input").val().trim();
       console.log(addedGif);
-      // Adding movie from the textbox to our array
+      
+      // Adding new GIF to  gifArray
       gifArray.push(addedGif);
 
-
+      //call to GIPHY for GIF
+      ajaxCall();
       console.log(queryURL);
+    
+      // This will create, and append the buttons to div
+      renderButtons();
+    });
+
+ // handles events where any of the buttons in the buttons-view div are pressed
+  
+      $("#buttons-view").on("click", function(event) {
+      event.preventDefault();
+      addedGif = $(this).attr("data-name");
+        console.log(addedGif + " on button click");
+
+      //call to GIPHY for GIF  
+      ajaxCall();
+
+     });
+
+
+
+function ajaxCall() {
+
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        console.log(gifSearch);
-        console.log(response);
-    
-      });
-  
-      // Calling renderButtons which handles the processing of our movie array
-      renderButtons();
-    });
 
- // handles events where any of the buttons are pressed
-     $(".gif-btn").on("click", function(event) {
-      event.preventDefault();
+        console.log(response);
+        
+        // Saving the  url
+          var imageUrl = response.data[0].images.downsized_small.mp4;
+        
+          // Creating and storing an image tag
+          var gifImage = $("<img>");
+
+          // Setting the catImage src attribute to imageUrl
+          gifImage.attr("src", imageUrl);
+          console.log(imageUrl);
+          gifImage.attr("alt", "gif image");
+
+          // Prepending the catImage to the images div
+          console.log(gifImage);
+          $("#gif-view").prepend(gifImage);
+       });
+    
+      }
+  
+
+//function appendImages() {
+
+
     
 
  
-      // Function for displaying movie data
+      // Function for displaying initial Array of buttons
       function renderButtons() {
 
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
+        //delete array of buttons each time
         $("#buttons-view").empty();
        // $("<input>").empty();
 
-        // Looping through the array of movies
+        // Looping through the array of Gifs
         for (var i = 0; i < gifArray.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          // Then dynamicaly generating buttons for each Gif listed in the array
+          
           var a = $("<button>");
           // Adding a class of movie-btn to our button
           a.addClass("gif-btn");
@@ -66,15 +101,11 @@ $(document).ready(function () {
           $("#buttons-view").append(a);
         }
       }
-
- 
+    
   
-     // Adding a click event listener to all elements with a class of "movie-btn"
-    // $(document).on("click", ".movie-btn", displayMovieInfo);
-  
-     // Calling the renderButtons function to display the intial buttons
+     
+     // Calling the renderButtons function to display the intial buttons           
      renderButtons();
   
-});
-
+}); 
 
