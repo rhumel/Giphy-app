f65179c73ab19a34d95c677b959f31cee050ec97
rhumel <rhumel@hotmail.com>
2018-07-13 12:20:49 -0400

    corrected animate/still code

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -4,10 +4,6 @@ $(document).ready(function () {
  var gifArray = ["Dog", "Cat", "Hello", "Happy Birtday"];
  var addedGif;  
 
- 
-
-  
-
 
   // handles events where a GIF Search button is clicked
     $("#add-gif").on("click", function(event) {
@@ -39,7 +35,20 @@ $(document).ready(function () {
 
      });
 
-
+// handle click events on gifs
+$(document).on("click", ".gif", function() {
+  // set the state value s/b either animate or still
+  var state = $(this).attr("data-state");
+  console.log(state);
+  // alternate between animate and still gif when clicked
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
+});
 
 function ajaxCall(query) {
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=HkjK4ZbPCa08m82JFOGCEzAuICSYLnwQ&limit=1&offset=0&rating=G&lang=en&q=" + query;
@@ -52,14 +61,24 @@ function ajaxCall(query) {
         
         // Saving the  url
           var imageUrl = response.data[0].images.fixed_height.url;
-        console.log(imageUrl + "just saved link from object");
+         //added for start and stop
+          var animateUrl = response.data[0].images.fixed_height.url;
+          var stillUrl = response.data[0].images.fixed_height_still.url;
+         
+          console.log(imageUrl + "just saved link from object");
           // Creating and storing an image tag
           var gifImage = $("<img>");
 
-          // Setting the catImage src attribute to imageUrl
+          // Setting the gif  src attribute to imageUrl
           gifImage.attr("src", imageUrl);
           console.log(imageUrl);
           gifImage.attr("alt", "gif image");
+         //adding for the start and stop
+          gifImage.attr("data-still",stillUrl);
+          gifImage.attr("data-animate",animateUrl);
+          gifImage.attr("data-state", "animate");
+          gifImage.addClass("gif");
+
 
           // Prepending the catImage to the images div
           console.log(gifImage);
@@ -87,15 +106,15 @@ function ajaxCall(query) {
 
           // Then dynamicaly generating buttons for each Gif listed in the array
           
-          var a = $("<button>");
+          var button = $("<button>");
           // Adding a class of movie-btn to our button
-          a.addClass("gif-btn");
+          button.addClass("gif-btn");
           // Adding a data-attribute
-          a.attr("data-name", gifArray[i]);
+          button.attr("data-name", gifArray[i]);
           // Providing the initial button text
-          a.text(gifArray[i]);
+          button.text(gifArray[i]);
           // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
+          $("#buttons-view").append(button);
 
          
         }
